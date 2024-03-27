90f0296c2754ace4b565af54181cd0def153bf2e
5v3nn <sven.greuter@protonmail.ch>
2024-03-27 15:15:57 +0100

    find user by token in user repo
    sopra-fs24-group07/sopra-fs24-group-07-server#82
    - needed for auth service

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs24.repository;
 
 import ch.uzh.ifi.hase.soprafs24.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   User findByName(String name);
 
   User findByUsername(String username);
+
+  User findByToken(String token);
 }
