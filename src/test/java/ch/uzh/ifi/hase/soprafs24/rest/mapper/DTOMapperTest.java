30de4bc59407ca61c9c63070f7e2ca6f9c704f5b
5v3nn <sven.greuter@protonmail.ch>
2024-03-29 09:58:47 +0100

    check user psw on dto mapping
    sopra-fs24-group07/sopra-fs24-group-07-server#20

--- src/test/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapperTest.java
@@ -22,6 +22,7 @@ public class DTOMapperTest {
     UserPostDTO userPostDTO = new UserPostDTO();
     userPostDTO.setName("name");
     userPostDTO.setUsername("username");
+    userPostDTO.setPassword("password");
 
     // MAP -> Create user
     User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
@@ -29,6 +30,7 @@ public class DTOMapperTest {
     // check content
     assertEquals(userPostDTO.getName(), user.getName());
     assertEquals(userPostDTO.getUsername(), user.getUsername());
+    assertEquals(userPostDTO.getPassword(), user.getPassword());
   }
 
   @Test
