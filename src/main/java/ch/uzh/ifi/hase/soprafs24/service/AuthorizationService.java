30c956d69319a0b31a914b0154f17c750b784698
5v3nn <sven.greuter@protonmail.ch>
2024-03-30 14:43:47 +0100

    improvement monitoring login/auth
    sopra-fs24-group07/sopra-fs24-group-07-server#82

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/AuthorizationService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/AuthorizationService.java
@@ -32,6 +32,8 @@ public class AuthorizationService {
    * @return the token of the user, null otherwise
    */
   public String login(String username, String password) {
+    log.info("Trying to log in user '{}'", username); // monitor login attempts
+
     User foundUser = userRepository.findByUsername(username);
     return foundUser != null && foundUser.getPassword().equals(password) ? foundUser.getToken()
                                                                          : null;
@@ -44,6 +46,8 @@ public class AuthorizationService {
    * @throws ResponseStatusException if the token is invalid
    */
   public void isAuthorized(String token) {
+    log.info("Checking authorization for token '{}'", token); // monitor authorization attempts
+
     if (this.userRepository.findByToken(token) == null) {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
     }
@@ -57,6 +61,8 @@ public class AuthorizationService {
    * @throws ResponseStatusException if the token is invalid or does not belong to the user
    */
   public void isAuthorized(String token, String username) {
+    log.info("Checking authorization for token '{}'", token); // monitor authorization attempts
+
     User foundUser = userRepository.findByToken(token);
     if (foundUser == null || !foundUser.getUsername().equals(username)) {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
