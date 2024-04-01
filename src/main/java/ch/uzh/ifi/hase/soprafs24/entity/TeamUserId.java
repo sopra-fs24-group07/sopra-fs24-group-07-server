ee0dbfdde45264240f09a4cc53d25f13d00a7cf9
5v3nn <sven.greuter@protonmail.ch>
2024-04-01 17:33:20 +0200

    added serialversionUID to teamuserid
    sopra-fs24-group07/sopra-fs24-group-07-server#86
    - just in case

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/TeamUserId.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/TeamUserId.java
@@ -10,6 +10,8 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class TeamUserId implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   private Long teamId;
 
   private Long userId;
