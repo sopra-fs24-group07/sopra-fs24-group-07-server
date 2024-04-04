d88c748ac60182debdef0c480154d85b78713978
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-04 21:07:08 +0200

    Creation and modification of this file for Post DTOs of Task
    Sopra-fs24-group07/sopra-fs24-group-07-server#33
    -TaskPostDTO

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/TaskPostDTO.java
@@ -0,0 +1,43 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+import ch.uzh.ifi.hase.soprafs24.constant.TaskStatus;
+
+public class TaskPostDTO {
+    private Integer teamId;
+    private String title;
+    private String description;
+    private TaskStatus status;
+
+    public Integer getTeamkId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+}
