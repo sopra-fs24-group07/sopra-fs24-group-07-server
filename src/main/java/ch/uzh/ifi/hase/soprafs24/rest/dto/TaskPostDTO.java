097218af9662c6a4467e36ab49baad33193fea3c
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-05 13:37:50 +0200

    Creation of POST DTO
    Sopra-fs24-group07/sopra-fs24-group-07-server#33
    -TaskPostDTO.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/TaskPostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/TaskPostDTO.java
@@ -3,41 +3,40 @@ package ch.uzh.ifi.hase.soprafs24.rest.dto;
 import ch.uzh.ifi.hase.soprafs24.constant.TaskStatus;
 
 public class TaskPostDTO {
-    private Integer teamId;
-    private String title;
-    private String description;
-    private TaskStatus status;
+  private Integer teamId;
+  private String title;
+  private String description;
+  private TaskStatus status;
 
-    public Integer getTeamkId() {
-        return teamId;
-    }
+  public Integer getTeamkId() {
+    return teamId;
+  }
 
-    public void setTeamId(Integer teamId) {
-        this.teamId = teamId;
-    }
+  public void setTeamId(Integer teamId) {
+    this.teamId = teamId;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public TaskStatus getStatus() {
-        return status;
-    }
+  public TaskStatus getStatus() {
+    return status;
+  }
 
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
+  public void setStatus(TaskStatus status) {
+    this.status = status;
+  }
 }
