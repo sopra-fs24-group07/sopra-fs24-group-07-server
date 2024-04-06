f68bd2dcbe9caa1555ab951ef1f2bb1b79009289
5v3nn <sven.greuter@protonmail.ch>
2024-04-06 22:33:34 +0200

    fix tests and repository for testing
    sopra-fs24-group07/sopra-fs24-group-07-server#80

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
  * the primary key
  */
 @Entity
-@Table(name = "USER")
+@Table(name = "USERS")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
 
