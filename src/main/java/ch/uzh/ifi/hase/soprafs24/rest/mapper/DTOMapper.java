017c9a53d7c3c1519a45f34561fcbae365239e13
5v3nn <sven.greuter@protonmail.ch>
2024-04-17 08:34:37 +0200

    session get datetimes formated
    sopra-fs24-group07/sopra-fs24-group-07-server#71

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapper.java
@@ -5,6 +5,8 @@ import ch.uzh.ifi.hase.soprafs24.entity.Task;
 import ch.uzh.ifi.hase.soprafs24.entity.Team;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
@@ -63,4 +65,16 @@ public interface DTOMapper {
 
   @Mapping(source = "goalMinutes", target = "goalMinutes")
   Session convertSessionPostDTOtoEntity(SessionPostDTO sessionPostDTO);
+
+  @Mapping(source = "sessionId", target = "sessionId")
+  @Mapping(
+      source = "startDateTime", target = "startDateTime", qualifiedByName = "formatLocalDateTime")
+  @Mapping(source = "endDateTime", target = "endDateTime", qualifiedByName = "formatLocalDateTime")
+  @Mapping(source = "goalMinutes", target = "goalMinutes")
+  SessionGetDTO
+  convertEntityToSessionGetDTO(Session session);
+
+  default String formatLocalDateTime(LocalDateTime localDateTime) {
+    return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+  }
 }
