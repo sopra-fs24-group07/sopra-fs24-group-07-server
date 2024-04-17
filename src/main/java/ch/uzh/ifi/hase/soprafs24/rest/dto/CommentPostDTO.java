c2ea0eb7dec50348c676ec9a059149ef9d73ac2c
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-17 22:19:27 +0200

    Created PostDTO file for Comments
    Sopra-fs24-group07/sopra-fs24-group-07-server#40
    -CommentPostDTO.java

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/CommentPostDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+public class CommentPostDTO {
+  private String text;
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+}
