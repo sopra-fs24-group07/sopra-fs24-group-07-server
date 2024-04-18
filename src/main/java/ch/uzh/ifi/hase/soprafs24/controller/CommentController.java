45bd1ff4d838be15c317f59ea9add08149a8de7f
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-18 19:40:58 +0200

    Problems solved
    Sopra-fs24-group07/sopra-fs24-group-07-server#40
    -CommentController.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/CommentController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/CommentController.java
@@ -1,13 +1,11 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Comment;
-import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.CommentGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.CommentPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.AuthorizationService;
 import ch.uzh.ifi.hase.soprafs24.service.CommentService;
-import ch.uzh.ifi.hase.soprafs24.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +14,10 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
   private final CommentService commentService;
   private final AuthorizationService authorizationService;
-  private final TaskService taskService;
 
-  CommentController(CommentService commentService, AuthorizationService authorizationService,
-      TaskService taskService) {
+  CommentController(CommentService commentService, AuthorizationService authorizationService) {
     this.commentService = commentService;
     this.authorizationService = authorizationService;
-    this.taskService = taskService;
   }
 
   @PostMapping
@@ -30,14 +25,15 @@ public class CommentController {
   @ResponseBody
   public CommentGetDTO createComment(@PathVariable Long teamId, @PathVariable Long taskId,
       @RequestBody CommentPostDTO commentPostDTO, @RequestHeader("Authorization") String token) {
-    User authorizedUser = authorizationService.isAuthorized(token);
-
     Comment commentInput = DTOMapper.INSTANCE.convertCommentPostDTOtoEntity(commentPostDTO);
-    commentInput.setTask(taskService.getTask(taskId));
-    commentInput.setUser(authorizedUser); // Set the user of the comment
 
-    Comment createdComment = commentService.createComment(commentInput);
+    // Check if the user exists and if the token belongs to that user.
+    authorizationService.isExistingAndAuthorized(token, commentInput.getUser().getUserId());
 
+    // Create the comment
+    Comment createdComment = commentService.createComment(commentInput, taskId);
+
+    // convert internal representation of comment back to API
     return DTOMapper.INSTANCE.convertEntityToCommentGetDTO(createdComment);
   }
 }
