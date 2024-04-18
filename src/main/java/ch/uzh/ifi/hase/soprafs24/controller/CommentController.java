e5938a49cf126d7c81f8df94ee334996d8f68b54
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-19 00:20:24 +0200

    GET methods for comment in controller
    Sopra-fs24-group07/sopra-fs24-group-07-server#40
    -CommentController.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/CommentController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/CommentController.java
@@ -7,6 +7,8 @@ import ch.uzh.ifi.hase.soprafs24.rest.dto.CommentPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.AuthorizationService;
 import ch.uzh.ifi.hase.soprafs24.service.CommentService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,5 +40,22 @@ public class CommentController {
 
     // Convert internal representation of comment back to API
     return DTOMapper.INSTANCE.convertEntityToCommentGetDTO(createdComment);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<CommentGetDTO> getComments(@PathVariable Long teamId, @PathVariable Long taskId,
+      @RequestHeader("Authorization") String token) {
+    // Get the user for that user token in header and check if that token is in team
+    authorizationService.isAuthorizedAndBelongsToTeam(token, teamId);
+
+    // Get comments
+    List<Comment> comments = commentService.getCommentsByTaskId(taskId);
+
+    // Convert internal representation of comments back to API
+    return comments.stream()
+        .map(DTOMapper.INSTANCE::convertEntityToCommentGetDTO)
+        .collect(Collectors.toList());
   }
 }
