d9d5d074714f8ac79a60e74f1244f900e0aa63aa
Alihan26 <alihan.karatasli@hotmail.com>
2024-04-19 20:47:46 +0200

    Integration Service Test fixed for comment
    Sopra-fs24-group07/sopra-fs24-group-07-server#42
    -Integrationn Service Test

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/CommentServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/CommentServiceIntegrationTest.java
@@ -2,11 +2,14 @@ package ch.uzh.ifi.hase.soprafs24.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import ch.uzh.ifi.hase.soprafs24.constant.TaskStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.Comment;
 import ch.uzh.ifi.hase.soprafs24.entity.Task;
+import ch.uzh.ifi.hase.soprafs24.entity.Team;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.repository.CommentRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.TaskRepository;
+import ch.uzh.ifi.hase.soprafs24.repository.TeamRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,6 +31,8 @@ public class CommentServiceIntegrationTest {
 
   @Qualifier("taskRepository") @Autowired private TaskRepository taskRepository;
 
+  @Qualifier("teamRepository") @Autowired private TeamRepository teamRepository;
+
   @Qualifier("userRepository") @Autowired private UserRepository userRepository;
 
   @Autowired private CommentService commentService;
@@ -36,38 +41,51 @@ public class CommentServiceIntegrationTest {
   public void setup() {
     commentRepository.deleteAll();
     taskRepository.deleteAll();
+    teamRepository.deleteAll();
     userRepository.deleteAll();
   }
 
-  // @Test
-  // public void createComment_validInputs_success() {
-  //   // given a task
-  //   Task testTask = new Task();
-  //   testTask.setTitle("Test Task");
-  //   testTask.setDescription("This is a task for testing");
-  //   taskRepository.saveAndFlush(testTask);
+  @Test
+  public void createComment_validInputs_success() {
+    String justiceTeam = "productiviteam";
 
-  //   // given a user
-  //   User testUser = new User();
-  //   testUser.setUsername("testUser");
-  //   testUser.setName("Test User");
-  //   testUser.setPassword("password123");
-  //   testUser.setToken("1");
-  //   userRepository.saveAndFlush(testUser);
+    // given
+    Team testTeam1 = new Team();
+    testTeam1.setName(justiceTeam);
+    testTeam1.setDescription("We are a productive team!");
+    testTeam1.setTeamUUID("team-uuid");
+    teamRepository.saveAndFlush(testTeam1);
 
-  //   // given a comment
-  //   Comment testComment = new Comment();
-  //   testComment.setText("This is a test comment");
-  //   testComment.setTask(testTask);
-  //   testComment.setUser(testUser);
+    // given a task
+    Task testTask = new Task();
+    testTask.setTitle("Test Task");
+    testTask.setDescription("This is a task for testing");
+    testTask.setStatus(TaskStatus.TODO);
+    testTask.setTeam(testTeam1);
+    taskRepository.saveAndFlush(testTask);
 
-  //   // when
-  //   Comment createdComment = commentService.createComment(testComment, testTask.getTaskId());
+    // given a user
+    User testUser = new User();
+    testUser.setUsername("testUser");
+    testUser.setName("Test User");
+    testUser.setPassword("password123");
+    testUser.setToken("1");
+    userRepository.saveAndFlush(testUser);
 
-  //   // then
-  //   assertNotNull(createdComment.getCreationDate());
-  //   assertEquals(testComment.getText(), createdComment.getText());
-  //   assertEquals(testTask.getTaskId(), createdComment.getTask().getTaskId());
-  //   assertEquals(testUser.getUserId(), createdComment.getUser().getUserId());
-  // }
+    // given a comment
+    Comment testComment = new Comment();
+    testComment.setText("This is a test comment");
+    testComment.setTask(testTask);
+    testComment.setUser(testUser);
+
+    // when
+    Comment createdComment = commentService.createComment(testComment, testTask.getTaskId());
+
+    // then
+    assertNotNull(createdComment.getCreationDate());
+
+    assertEquals(testComment.getText(), createdComment.getText());
+    assertEquals(testTask.getTaskId(), createdComment.getTask().getTaskId());
+    assertEquals(testUser.getUserId(), createdComment.getUser().getUserId());
+  }
 }
