19298fa5fb4f8883119c8095f76cf2c8c7c36bad
5v3nn <sven.greuter@protonmail.ch>
2024-04-20 09:19:44 +0200

    test more edge cases
    sopra-fs24-group07/sopra-fs24-group-07-server#42

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/CommentServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/CommentServiceTest.java
@@ -57,7 +57,7 @@ public class CommentServiceTest {
     Mockito.when(commentRepository.save(Mockito.any())).thenReturn(testComment);
   }
 
-  // POST
+  // region createComment tests
   /**
    * Test for creating a new comment with valid inputs
    */
@@ -110,8 +110,24 @@ public class CommentServiceTest {
         () -> commentService.createComment(emptyComment, testTask.getTaskId()));
   }
 
-  // GET
+  /**
+   * Test if sql exception is thrown when saving comment
+   */
+  @Test
+  public void createComment_sqlException_throwsException() {
+    // given -> an sql exception is thrown when the commentRepository save function is called
+    Mockito.when(commentRepository.save(Mockito.any()))
+        .thenThrow(
+            new RuntimeException("SQL Exception")); // some sql exception (integrity constraint)
 
+    // when/then -> try to create comment -> then the exception is thrown
+    assertThrows(ResponseStatusException.class,
+        () -> commentService.createComment(testComment, testTask.getTaskId()));
+  }
+
+  // endregion
+
+  // region getCommentsByTaskId tests
   /**
    * Test for getting all comments of a task if task exists and has comments
    */
@@ -163,4 +179,21 @@ public class CommentServiceTest {
     assertThrows(ResponseStatusException.class,
         () -> commentService.getCommentsByTaskId(testTask.getTaskId()));
   }
+
+  /**
+   * Test if the findByTask has a null return value
+   */
+  @Test
+  public void getCommentsByTaskId_nullComments_throwsException() {
+    // when -> try to find taskId in the taskService -> return dummy task
+    Mockito.when(taskService.getTask(Mockito.any())).thenReturn(testTask);
+
+    // when -> try to find comments by task in the commentRepository -> return null
+    Mockito.when(commentRepository.findByTask(Mockito.any())).thenReturn(null);
+
+    // call the method under test and assert an exception is thrown
+    assertThrows(ResponseStatusException.class,
+        () -> commentService.getCommentsByTaskId(testTask.getTaskId()));
+  }
+  // endregion
 }
