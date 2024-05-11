fb76d1157f7dcf58be544ef427435d418f266047
Alihan26 <alihan.karatasli@hotmail.com>
2024-05-11 15:23:14 +0200

    precommit
    Sopra-fs24-group07/sopra-fs24-group-07-server#195

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-
 @Configuration
 public class WebConfig {
   @Bean
@@ -12,4 +11,3 @@ public class WebConfig {
     return WebClient.create();
   }
 }
- 
