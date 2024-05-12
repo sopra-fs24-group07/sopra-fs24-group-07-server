867535aeec3422141009fa8818dec8ebc68e1f04
Alihan26 <alihan.karatasli@hotmail.com>
2024-05-12 13:43:06 +0200

    Controller covers edge cases of the service
    Sopra-fs24-group07/sopra-fs24-group-07-server#196
    -AIController.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/AIController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/AIController.java
@@ -3,8 +3,11 @@ package ch.uzh.ifi.hase.soprafs24.controller;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.service.AIService;
 import ch.uzh.ifi.hase.soprafs24.service.AuthorizationService;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/ai")
@@ -20,11 +23,23 @@ public class AIController {
   @PostMapping("/gpt-3.5-turbo-instruct")
   public ResponseEntity<String> callGpt35Instruct(
       @RequestBody String requestBody, @RequestHeader("Authorization") String token) {
+    // check if user is authorized (valid token)
     User authorizedUser = authorizationService.isAuthorized(token);
+
+    if (authorizedUser == null) {
+      return new ResponseEntity<>("User is not authorized", HttpStatus.UNAUTHORIZED);
+    }
+
     String cleanRequestBody = requestBody.replace("\"", "");
     String prompt = "Please write a very short poem about a team named" + cleanRequestBody;
-    String description = aiService.generateDescription(prompt);
-    System.out.println("The description has been set to: " + description);
-    return ResponseEntity.ok(description);
+
+    // Call the AI service to generate a description
+    Optional<String> description = aiService.generateDescription(prompt);
+    if (!description.isPresent()) {
+      return new ResponseEntity<>("Unable to generate description", HttpStatus.BAD_REQUEST);
+    }
+
+    System.out.println("The description has been set to: " + description.get());
+    return new ResponseEntity<>(description.get(), HttpStatus.OK);
   }
 }
