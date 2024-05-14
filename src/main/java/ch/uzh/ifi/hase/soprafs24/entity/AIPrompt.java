e6d4f134918d4d4a892973e4a291d4c561c5f4d1
Alihan26 <alihan.karatasli@hotmail.com>
2024-05-14 16:19:02 +0200

    The entity for DTO Mapping
    Sopra-fs24-group07/sopra-fs24-group-07-server#196

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/AIPrompt.java
@@ -0,0 +1,22 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+public class AIPrompt {
+  private String prompt;
+  private String answer;
+
+  public String getPrompt() {
+    return prompt;
+  }
+
+  public void setPrompt(String prompt) {
+    this.prompt = prompt;
+  }
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/AIPromptTeamDescriptionPostDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+public class AIPromptTeamDescriptionPostDTO {
+  private String promptParameter;
+
+  public String getPromptParameter() {
+    return promptParameter;
+  }
+
+  public void setPromptParameter(String promptParameter) {
+    this.promptParameter = promptParameter;
+  }
+}
